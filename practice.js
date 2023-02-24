6fddafcaa86d1cadcf88e5fcfa1ff276cee1f29e
23yUJeong <seoyujeong1234@gmail.com>
2023-02-25 02:35:01 +0900

    📝 : memo : 문서 추가

---
+++ practice.js
@@ -0,0 +1,28 @@
+// let password = "123" + 1;
+
+// console.log(password); // "1231"
+
+let score = 123 + 1;
+
+console.log(score); // 124
+
+console.log(6 % 2); // % 나머지 // 1
+console.log(1 % 2);
+
+let password = 1;
+password = password > 2;
+
+let judge = password <= 2;
+
+console.log(password); // false
+console.log(judge); // true
+
+console.log(a); // undefined
+var a = 1;
+console.log(a); // 1
+
+console.log("for문");
+for (var i = 1; i < 5; i++) {
+  console.log(i); // 1 // 2 // 3 // 4
+}
+console.log(i); // 5
