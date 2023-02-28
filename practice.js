a543e4a63aebf33ec18c5653effc85771b96633e
23yUJeong <seoyujeong1234@gmail.com>
2023-03-01 04:48:49 +0900

    📝 : memo : 문서 추가

--- practice.js
+++ practice.js
@@ -26,3 +26,33 @@ for (var i = 1; i < 5; i++) {
   console.log(i); // 1 // 2 // 3 // 4
 }
 console.log(i); // 5
+
+let fruits = ["banana", "apple", "mango", "grape"];
+
+console.log(fruits); // ['banana', 'apple', 'mango', 'grape']
+console.log(fruits[0]); // "banana"
+
+fruits[0] = "orange";
+
+console.log(fruits[0]); // "orange"
+
+fruits.pop();
+
+console.log(fruits); // ['orange', 'apple', 'mango']
+
+fruits.push("pineapple");
+
+console.log(fruits); // ['orange', 'apple', 'mango', 'pineapple']
+
+console.log(fruits.includes("apple")); // true
+
+console.log(fruits.includes("carrot")); // false
+
+console.log(fruits.indexOf("mango")); // 2
+
+// console.log(fruits.slice(2)); // ['mango', 'pineapple']
+
+// console.log(fruits.slice(1, 3)); // ['apple', 'mango']
+
+fruits.splice(2, 1);
+console.log(fruits); // ['orange', 'apple', 'pineapple']
--- problems.js
+++ problems.js
@@ -65,3 +65,236 @@ console.log(true && true);
 console.log(true && false);
 console.log(!true);
 console.log(!false);
+
+// 배열 문제
+let animals = [
+  "Aardvark",
+
+  "Albatross",
+
+  "Alligator",
+
+  "Alpaca",
+
+  "Ant",
+
+  "Ape",
+
+  "Armadillo",
+
+  "Donkey",
+
+  "Baboon",
+
+  "Badger",
+
+  "Barracuda",
+
+  "Bat",
+
+  "Bear",
+
+  "Beaver",
+
+  "Bee",
+
+  "Bison",
+
+  "Cat",
+
+  "Caterpillar",
+
+  "Cattle",
+
+  "Chamois",
+
+  "Cheetah",
+
+  "Chicken",
+
+  "Chimpanzee",
+
+  "Chinchilla",
+
+  "Chough",
+
+  "Clam",
+
+  "Cobra",
+
+  "Cockroach",
+
+  "Cod",
+
+  "Cormorant",
+
+  "Dugong",
+
+  "Dunlin",
+
+  "Eagle",
+
+  "Echidna",
+
+  "Eel",
+
+  "Eland",
+
+  "Elephant",
+
+  "Elk",
+
+  "Emu",
+
+  "Falcon",
+
+  "Ferret",
+
+  "Finch",
+
+  "Fish",
+
+  "Flamingo",
+
+  "Fly",
+
+  "Fox",
+
+  "Frog",
+
+  "Gaur",
+
+  "Gazelle",
+
+  "Gerbil",
+
+  "Giraffe",
+
+  "Grasshopper",
+
+  "Heron",
+
+  "Herring",
+
+  "Hippopotamus",
+
+  "Hornet",
+
+  "Horse",
+
+  "Kangaroo",
+
+  "Kingfisher",
+
+  "Koala",
+
+  "Kookabura",
+
+  "Moose",
+
+  "Narwhal",
+
+  "Newt",
+
+  "Nightingale",
+
+  "Octopus",
+
+  "Okapi",
+
+  "Opossum",
+
+  "Quail",
+
+  "Quelea",
+
+  "Quetzal",
+
+  "Rabbit",
+
+  "Raccoon",
+
+  "Rail",
+
+  "Ram",
+
+  "Rat",
+
+  "Raven",
+
+  "Red deer",
+
+  "Sandpiper",
+
+  "Sardine",
+
+  "Sparrow",
+
+  "Spider",
+
+  "Spoonbill",
+
+  "Squid",
+
+  "Squirrel",
+
+  "Starling",
+
+  "Stingray",
+
+  "Tiger",
+
+  "Toad",
+
+  "Whale",
+
+  "Wildcat",
+
+  "Wolf",
+
+  "Worm",
+
+  "Wren",
+
+  "Yak",
+
+  "Zebra",
+];
+
+/* 1. "Zebra" 제거하기 */
+// animals.pop();
+// console.log(animals); // ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Ape', 'Armadillo', 'Donkey', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Cat', 'Caterpillar', 'Cattle', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'Giraffe', 'Grasshopper', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Kangaroo', 'Kingfisher', 'Koala', 'Kookabura', 'Moose', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Red deer', 'Sandpiper', 'Sardine', 'Sparrow', 'Spider', 'Spoonbill', 'Squid', 'Squirrel', 'Starling', 'Stingray', 'Tiger', 'Toad', 'Whale', 'Wildcat', 'Wolf', 'Worm', 'Wren', 'Yak']
+
+/* 2. Dog 추가하기 */
+// animals.push("Dog");
+// console.log(animals); // ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Ape', 'Armadillo', 'Donkey', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Cat', 'Caterpillar', 'Cattle', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'Giraffe', 'Grasshopper', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Kangaroo', 'Kingfisher', 'Koala', 'Kookabura', 'Moose', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Red deer', 'Sandpiper', 'Sardine', 'Sparrow', 'Spider', 'Spoonbill', 'Squid', 'Squirrel', 'Starling', 'Stingray', 'Tiger', 'Toad', 'Whale', 'Wildcat', 'Wolf', 'Worm', 'Wren', 'Yak', 'Zebra', 'Dog']
+
+/* 3. "Mosquito", "Mouse", "Mule" 추가하기 */
+// animals.push("Mosquito", "Mouse", "Mule");
+// console.log(animals); // ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Ape', 'Armadillo', 'Donkey', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Cat', 'Caterpillar', 'Cattle', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'Giraffe', 'Grasshopper', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Kangaroo', 'Kingfisher', 'Koala', 'Kookabura', 'Moose', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Red deer', 'Sandpiper', 'Sardine', 'Sparrow', 'Spider', 'Spoonbill', 'Squid', 'Squirrel', 'Starling', 'Stingray', 'Tiger', 'Toad', 'Whale', 'Wildcat', 'Wolf', 'Worm', 'Wren', 'Yak', 'Zebra', 'Mosquito', 'Mouse', 'Mule']
+
+/* 4. "Human" 포함되어 있는가? */
+console.log(animals.includes("Human")); // false
+
+/* 5. "Cat" 포함되어 있는가? */
+console.log(animals.includes("Cat")); // true
+
+/* 6. "Red deer"을 "Deer"로 바꾸시오 */
+// console.log(animals.indexOf("Red deer")); // 77
+// animals[77] = "Deer";
+// console.log(animals.includes("Deer")); // true
+
+/* 7. "Spider"부터 3개의 값을 기존 배열에서 제거하기 */
+// console.log(animals.indexOf("Spider")); // 81
+// animals.splice(81, 3);
+// console.log(animals); // ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Ape', 'Armadillo', 'Donkey', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Cat', 'Caterpillar', 'Cattle', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'Giraffe', 'Grasshopper', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Kangaroo', 'Kingfisher', 'Koala', 'Kookabura', 'Moose', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Red deer', 'Sandpiper', 'Sardine', 'Sparrow', 'Squirrel', 'Starling', 'Stingray', 'Tiger', 'Toad', 'Whale', 'Wildcat', 'Wolf', 'Worm', 'Wren', 'Yak', 'Zebra']
+
+/* 8. "Tiger" 이후의 값을 제거하기 */
+console.log(animals.indexOf("Tiger")); // 87
+animals.splice(87);
+console.log(animals); // ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Ape', 'Armadillo', 'Donkey', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Cat', 'Caterpillar', 'Cattle', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'Giraffe', 'Grasshopper', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Kangaroo', 'Kingfisher', 'Koala', 'Kookabura', 'Moose', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Red deer', 'Sandpiper', 'Sardine', 'Sparrow', 'Spider', 'Spoonbill', 'Squid', 'Squirrel', 'Starling', 'Stingray', 'Tiger']
+
+/* 9. "Baboon"부터 "Bison"까지 새로운 배열에 저장하기 */
+// console.log(animals.indexOf("Baboon")); // 8
+// console.log(animals.indexOf("Bison")); // 15
+// console.log(animals.slice(8, 16)); // ['Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison']
